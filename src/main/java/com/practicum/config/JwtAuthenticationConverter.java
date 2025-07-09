b50b581bd127c392cfe47645162627efcb3ad3c0
Irvin Celi <ioceli@utpl.edu.ec>
2025-07-09 17:04:10 -0500

    JwtAuthenticationConverter added

---
+++ src/main/java/com/practicum/config/JwtAuthenticationConverter.java
@@ -0,0 +1,100 @@
+package com.practicum.config;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.databind.util.Converter;
+
+
+@Component
+public class JwtAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+   
+    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter =
+            new JwtGrantedAuthoritiesConverter();
+
+    @Value("${jwt.auth.converter.principle-attribute}")
+    private String principleAttribute;
+
+    @Value("${jwt.auth.converter.resource-id}")
+    private String resourceId;
+
+    @Override
+    public AbstractAuthenticationToken convert(@NonNull Jwt jwt) {
+
+        Collection<GrantedAuthority> authorities = Stream
+                .concat(jwtGrantedAuthoritiesConverter.convert(jwt).stream(), extractResourceRoles(jwt).stream())
+                .toList();
+
+        return new JwtAuthenticationToken(
+                jwt,
+                authorities,
+                getPrincipleClaimName(jwt)
+        );
+    }
+
+
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+
+        Map<String, Object> resourceAccess;
+        Map<String, Object> resource;
+        Collection<String> resourceRoles;
+
+        if (jwt.getClaim("resource_access") == null) {
+            return Set.of();
+        }
+        resourceAccess = jwt.getClaim("resource_access");
+
+        if (resourceAccess.get(resourceId) == null) {
+            return Set.of();
+        }
+
+        resource = (Map<String, Object>) resourceAccess.get(resourceId);
+
+        resourceRoles = (Collection<String>) resource.get("roles");
+        return resourceRoles
+                .stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toSet());
+    }
+
+
+    private String getPrincipleClaimName(Jwt jwt) {
+        String claimName = JwtClaimNames.SUB;
+        if (principleAttribute != null) {
+            claimName = principleAttribute;
+        }
+        return jwt.getClaim(claimName);
+    }
+
+
+    @Override
+    public JavaType getInputType(TypeFactory arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getInputType'");
+    }
+
+
+    @Override
+    public JavaType getOutputType(TypeFactory arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getOutputType'");
+    }
+    
+}
